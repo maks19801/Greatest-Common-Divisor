929c04a4a14dc7e5d064865c071c6f25418afbee
Порховский Максим <maks19801@gmail.com>
2021-01-14 13:28:02 +0200

    Add project files.

---
+++ GreatestCommonDivision/GreatestCommonDivisor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreatestCommonDivision
+{
+    class GreatestCommonDivisor
+    {
+        public static int GetGreatestCommonDivisorByDivision(int firstNumber, int secondNumber)
+        {
+            while (firstNumber != 0 && secondNumber != 0)
+            {
+                if (firstNumber > secondNumber)
+                {
+                    firstNumber = firstNumber % secondNumber;
+                }
+                else
+                {
+                    secondNumber = secondNumber % firstNumber;
+                }
+            }
+            return firstNumber + secondNumber;
+        }
+        public static int GetGreatestCommonDivisorBySubstraction(int firstNumber, int secondNumber)
+        {
+            while (firstNumber != 0 && secondNumber != 0)
+            {
+                if (firstNumber > secondNumber)
+                {
+                    firstNumber = firstNumber - secondNumber;
+                }
+                else
+                {
+                    secondNumber = secondNumber - firstNumber;
+                }
+            }
+            return firstNumber + secondNumber;
+        }
+        public static int GetGreatestCommonDivisorByRecursion(int firstNumber, int secondNumber)
+        {
+            if (firstNumber == 0)
+            {
+                return secondNumber;
+            }
+            else
+            {
+                return GetGreatestCommonDivisorByRecursion(secondNumber % firstNumber, firstNumber);
+            }
+        }
+
+        internal static int GetGreatestCommonDivisorByRecursionTernaryOperator(int firstNumber, int secondNumber)
+        {
+            return firstNumber != 0 ? GetGreatestCommonDivisorByRecursionTernaryOperator(secondNumber % firstNumber, firstNumber) : secondNumber;
+        }
+    }
+}
---
+++ GreatestCommonDivision/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GreatestCommonDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a program for calculating the greatest common division of two numbers!");
+            Console.WriteLine("Please enter two numbers");
+
+            Console.Write("Enter first number: ");
+            var firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            var secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The greatest common divisor of {firstNumber} and {secondNumber} is " 
+                + GreatestCommonDivisor.GetGreatestCommonDivisorByDivision(firstNumber, secondNumber));
+
+            Console.WriteLine($"The greatest common divisor of {firstNumber} and {secondNumber} is "
+                + GreatestCommonDivisor.GetGreatestCommonDivisorBySubstraction(firstNumber, secondNumber));
+
+            Console.WriteLine($"The greatest common divisor of {firstNumber} and {secondNumber} is "
+                + GreatestCommonDivisor.GetGreatestCommonDivisorByRecursion(firstNumber, secondNumber));
+
+            Console.WriteLine($"The greatest common divisor of {firstNumber} and {secondNumber} is "
+                + GreatestCommonDivisor.GetGreatestCommonDivisorByRecursionTernaryOperator(firstNumber, secondNumber));
+        }
+    }
+}
